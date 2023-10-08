1b6deb43a6f7d5f6cc05a82dc3bd1369e2ef7a00
Shariq@SD <shariqsd2003@gmail.com>
2023-10-08 22:51:15 +0530

    First & Final Commit

--- server/index.js
+++ server/index.js
@@ -10,54 +10,55 @@ const port = 3000;
 const MODEL_NAME = "models/chat-bison-001";
 const API_KEY = process.env.API_KEY;
 const client = new DiscussServiceClient({
-    authClient: new GoogleAuth().fromAPIKey(API_KEY),
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
 });
 
-const CONTEXT = "Respond to all questions with a rhyming poem.";
+const CONTEXT =
+  "Respond to all questions with atleast 300 words";
 const EXAMPLES = [
-    {
-        input: { content: "What is the capital of California?" },
-        output: {
-            content: `If the capital of California is what you seek,
+  {
+    input: { content: "What is the capital of California?" },
+    output: {
+      content: `If the capital of California is what you seek,
 Sacramento is where you ought to peek.`,
-        },
     },
+  },
 ];
 
 let messages = [];
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: "*",
-    })
+  cors({
+    origin: "*",
+  })
 );
 
 app.post("/api/chatbot", async (req, res) => {
-    const requestData = req.body;
+  const requestData = req.body;
 
-    if (requestData && requestData.message) {
-        const message = requestData.message;
-        messages.push({ content: message });
+  if (requestData && requestData.message) {
+    const message = requestData.message;
+    messages.push({ content: message });
 
-        const result = await client.generateMessage({
-            model: MODEL_NAME,
-            prompt: {
-                context: CONTEXT,
-                examples: EXAMPLES,
-                messages,
-            },
-        });
+    const result = await client.generateMessage({
+      model: MODEL_NAME,
+      prompt: {
+        context: CONTEXT,
+        examples: EXAMPLES,
+        messages,
+      },
+    });
 
-        const messageResult = result[0].candidates[0].content;
-        messages.push({ content: messageResult });
+    const messageResult = result[0].candidates[0].content;
+    messages.push({ content: messageResult });
 
-        res.json({ message: messageResult, agent: "chatbot" });
-    } else {
-        res.status(400).json({ error: "Content not provided" });
-    }
+    res.json({ message: messageResult, agent: "chatbot" });
+  } else {
+    res.status(400).json({ error: "Content not provided" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
